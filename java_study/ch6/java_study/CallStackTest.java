bff77c642bc7a8be973e8b072c1848f087873e52
Wan <can3719@naver.com>
2019-11-18 23:44:17 +0900

    20191118_java_study

---
+++ java_study/ch6/java_study/CallStackTest.java
@@ -0,0 +1,17 @@
+package java_study;
+
+public class CallStackTest {
+	
+	public static void main(String[] args) {
+		firstMethod();
+	}
+	
+	static void firstMethod() {
+		secondMethod();
+	}
+	
+	static void secondMethod() {
+		System.out.println("secondMethod()");
+	}
+
+}
