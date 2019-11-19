9b394c8e8dead2cb4c6c2de11d532b61d3bd0894
Jdesk <Jdesk@wan>
2019-11-19 18:24:20 +0900

    20191119_study

---
+++ java_study/ch6/java_study/FactorialTest.java
@@ -0,0 +1,21 @@
+package java_study;
+
+class FactorialTest {
+	
+	public static void main(String[] args) {
+		long result = factorial(4);
+		System.out.println(result);
+	}
+	
+	static long factorial(int n) {
+		long result=0;
+		if(n==1) {
+			result=1;
+		} else {
+			result = n*factorial(n-1);
+		}
+		
+		return result;
+	}
+
+}
