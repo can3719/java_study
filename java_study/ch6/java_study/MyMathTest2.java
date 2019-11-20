2cac1d237bc36d46a529e4c0caedd1a39213befd
Jdesk <Jdesk@wan>
2019-11-20 13:07:14 +0900

    20191120_company_study

--- java_study/ch6/java_study/MyMathTest2.java
+++ java_study/ch6/java_study/MyMathTest2.java
@@ -1,24 +1,8 @@
 package java_study;
 
-class MyMath2 {
-	long a, b;
-	
-	// 인스턴스변수 a, b만으르 이용해서 작업하므로 매개변수가 필요없다.
-	long add() { return a+b; }		// a, b는 인스턴스 변수
-	long subtract() { return a-b; }
-	long multiply() { return a*b; }
-	double divide() { return a/b; }
-	
-	// 인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
-	static long add (long a, long b) {return a+b; }
-	static long subtract (long a, long b) {return a-b; }
-	static long multiply (long a, long b) {return a*b; }
-	static double divide (double a, double b) {return a/b; }
-}
-
 class MyMathTest2 {
 	
-	public static void main(String args) {
+	public static void main(String[] args) {
 		// 클래스메서드 호출
 		System.out.println(MyMath2.add(200L, 100L));
 		System.out.println(MyMath2.subtract(200L, 100L));
@@ -36,3 +20,20 @@ class MyMathTest2 {
 	}
 
 }
+
+class MyMath2 {
+	long a, b;
+	
+	// 인스턴스변수 a, b만으르 이용해서 작업하므로 매개변수가 필요없다.
+	long add() { return a+b; }		// a, b는 인스턴스 변수
+	long subtract() { return a-b; }
+	long multiply() { return a*b; }
+	double divide() { return a/b; }
+	
+	// 인스턴스변수와 관계없이 매개변수만으로 작업이 가능하다.
+	static long add (long a, long b) {return a+b; }
+	static long subtract (long a, long b) {return a-b; }
+	static long multiply (long a, long b) {return a*b; }
+	static double divide (double a, double b) {return a/b; }
+}
+
