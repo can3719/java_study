d16d9ff9b6d66bb55aa4f78ebac8a6bfec07b7fe
Wan <can3719@naver.com>
2019-12-18 16:12:42 +0900

    20191217_home_study

---
+++ java_study/ch7/java_study/CaptionTvTest.java
@@ -0,0 +1,35 @@
+package java_study;
+
+class Tv {
+	boolean power;		// 전원상태 (on / off)
+	int channel;		// 채널
+	
+	void power() { power=!power; }
+	void channelUp() { ++channel; }
+	void channelDown() { --channel; }
+}
+
+class CaptionTv extends Tv {
+	boolean caption;	// 캡션상태 (on / off)
+	void displayCaption(String test) {
+		if(caption) {
+			System.out.println(test);
+		}
+	}
+}
+
+class CaptionTvTest {
+
+	public static void main(String args[]) {
+		CaptionTv ctv = new CaptionTv();
+		ctv.channel = 10;		// 조상 클래스로부터 상속받은 멤버
+		ctv.channelUp();		// 조상 클래스로부터 상속받은 멤버
+		System.out.println(ctv.channel);
+		
+		ctv.displayCaption("Hello, World");
+		ctv.caption=true;
+		ctv.displayCaption("Hello, world");
+		
+	}
+	
+}
