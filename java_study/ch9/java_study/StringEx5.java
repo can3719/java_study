7166deca1999038809675b940d49ddac9d0a3fe5
Wan <can3719@naver.com>
2020-02-03 23:12:03 +0900

    20200203_home_study

---
+++ java_study/ch9/java_study/StringEx5.java
@@ -0,0 +1,16 @@
+package java_study;
+
+public class StringEx5 {
+	static String s;
+	static String s2 = "";
+	
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++) {
+			s+=i;
+			s2+=i;
+		}
+		
+		System.out.println(s);
+		System.out.println(s2);
+	}
+}
---
+++ java_study/ch9/java_study/StringEx8.java
@@ -0,0 +1,17 @@
+package java_study;
+
+public class StringEx8 {
+	public static void main(String[] args) {
+		String[] number = {"1", "2", "3", "4", "5"};
+		String result1="";
+		int result2=0;
+		
+		for(int i=0; i<number.length; i++) {
+			result1+=number[i];
+			result2+=Integer.parseInt(number[i]);
+		}
+		
+		System.out.println("result1 : "+result1);
+		System.out.println("result2 : "+result2);
+	}
+}
