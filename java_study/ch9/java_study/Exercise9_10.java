188c163df36dba0b4350b2e53d95954b456ca8ab
Wan <can3719@naver.com>
2020-02-20 23:29:54 +0900

    20200220_home_study

---
+++ java_study/ch9/java_study/Exercise9_10.java
@@ -0,0 +1,47 @@
+package java_study;
+
+public class Exercise9_10 {
+	
+	public static String format(String str, int length, int alignment) {
+/*		
+		if(length<str.length()) {
+			return str.substring(0, length);
+		}
+*/		
+		
+		int diff = length-str.length();
+		if(diff<0) return str.substring(0, length);
+		
+		char[] source = str.toCharArray();
+		char[] result = new char[length];
+		
+		for(int i=0; i<result.length; i++) {
+			result[i]=' ';
+		}
+		
+		switch(alignment) {
+		case 0:
+		default:
+			System.arraycopy(source,  0,  result,  0,  source.length);
+			break;
+		case 1:
+			System.arraycopy(source,  0,  result,  diff/2, source.length);
+			break;
+		case 2:
+			System.arraycopy(source,  0,  result,  diff, source.length);
+			break;
+				
+		}
+		
+		return new String(result);
+	}
+	
+	public static void main(String[] args) {
+		String str = "가나다";
+		
+		System.out.println(format(str,7,0));	// 왼쪽정렬
+		System.out.println(format(str,7,1));	// 가운데정렬
+		System.out.println(format(str,7,2));	// 오른쪽정렬
+	}
+
+}
---
+++ java_study/ch9/java_study/Exercise9_9.java
@@ -0,0 +1,25 @@
+package java_study;
+
+public class Exercise9_9 {
+	
+	public static String delChar(String src, String delCh) {
+		StringBuffer sb = new StringBuffer(src.length());
+		
+		for(int i=0; i<src.length(); i++) {
+			char ch = src.charAt(i);
+			
+			// ch가 delCh에 포함되있지 않으면 sb에 추가
+			if(delCh.indexOf(ch)==-1)
+				sb.append(ch);
+		}
+		
+		return sb.toString();
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("(1!2@3^4~5)"+" ->"+delChar("(1!2@3^4~5)", "~!@#$%^&*()"));
+		System.out.println("(1 2   3   4\t5)"+"-> "+delChar("(1 2   3   4\t5)"," \t"));
+	}
+	
+}
