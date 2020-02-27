0054d9113aefa38786abf7b5dedf6eecffa0c143
Wan <can3719@naver.com>
2020-02-28 08:29:49 +0900

    20200228_company_study

---
+++ java_study/ch9/java_study/Exercise9_12.java
@@ -0,0 +1,13 @@
+package java_study;
+
+public class Exercise9_12 {
+	public static int getRand(int from, int to) {
+		return (int) (Math.random() * (Math.abs(to-from)+1))+Math.min(from, to);
+	}
+	
+	public static void main(String[] args) {
+		for(int i=0; i<20; i++) {
+			System.out.println(getRand(1,-3)+", ");
+		}
+	}
+}
